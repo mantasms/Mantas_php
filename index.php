3b04f9d46cb10c5ae7d13d8cb3761ebe64e24ecd
mantasms <m.sakaitis@gmail.com>
2018-10-16 12:56:18 +0300

    uzduotis #1

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML</title>
+  </head>
+  <h1> Mantas -HTML asas </h1>
+  <p> Moku kurti statini turini </p>
+</html>
+      <?php
 print 'Veikia su GIT!';
