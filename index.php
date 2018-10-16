61304a819abdc1b299a283c9ab45947cb6a7dfda
mantasms <m.sakaitis@gmail.com>
2018-10-16 14:07:04 +0300

    uzduotis #3

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<html lang="en" dir="ltr">
+<?php date_default_timezone_set("Europe/Vilnius");
+
+?>
+
+<html lang="en" dir="ltr">    
   <head>
     <meta charset="utf-8">
-    <title>As valdau HTML</title>
+    <title> <?php print 'Aš ' . date('l') . ' ir PHP'; ?> </title>
   </head>
-  <h1> Mantas -HTML asas </h1>
-  <p> Moku kurti statini turini </p>
+  
+  <h1> Mantas -HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date ('Y') . ' metų'; ?></h1>
+  <p> Viskas prasidėjo <?php print date('m') . ' mėnesio, ' . date('d') . ' dieną! '; ?> </p>
+  
 </html>
-      <?php
-print 'Veikia su GIT!';
