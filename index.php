0a75ac32d56ac00927424ea7b45aee0c295c29f3
mantasms <m.sakaitis@gmail.com>
2018-10-17 12:54:01 +0300

    Day 3 uzduotis #1

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 
-<?php date_default_timezone_set("Europe/Vilnius");
-// komentaras PHP
-
-?>
-
-<html lang="en" dir="ltr">   
-    <!-- komentaras-->
-  <head>
-    <meta charset="utf-8">
-    <title> PHP lydės ir <?php print date ('Y', strtotime('+' . rand (1, 10) . 'years')) . ' !'; ?> </title>
-  </head>
-  
-  <body style="background-color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>)">
+        <?php
+        $string = "tekstas";
+        $integer = 123;
+        $float = 1.234;
+        $boolean = true;
+        $null = null;
         
-      
-    <h1> Mantas - galbut turesiu <?php print rand (1, 5) . ' vaikus!'; ?> </h1>
-    <p> trampas nebebus prezidentu <?php print date('Y, m, d', strtotime('+' . rand (3, 10) . 'years')) ; ?> </p>
-    
-  </body>
-</html>
+        print " stringai - $string,<br> ineteger - $integer,<br> float - $float,<br> boolean - $boolean,<br> niekas - $null";
+        
+        ?>
