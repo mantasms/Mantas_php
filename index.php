8b77994f3d9e7a8eb1658a5ea955eb032967727f
mantasms <m.sakaitis@gmail.com>
2018-10-17 14:35:48 +0300

    Day 3 skaiciavimo uzduotis

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+<?php
+       
+$mano_pinigai = 1000;
+$spent_per_month = 600;
+$earned_per_month = 800;
+$unknown_per_month = rand(0, 1000);
+$months = 24;      
 
-        <?php
-        $string = "tekstas";
-        $integer = 123;
-        $float = 1.234;
-        $boolean = true;
-        $null = null;
+$prognoze = $mano_pinigai + ($earned_per_month - $spent_per_month - $unknown_per_month)*24;
+print "Po " . $months . " menesiu as turesiu " . $prognoze . " pinigu, kai random yra " . $unknown_per_month;
+
+print "<p>";
+
+$liepa = 31;
+$rugpjutis = 30;
+$rugsejis = 30;
+
+$siandien = date('d');
         
-        print " stringai - $string,<br> ineteger - $integer,<br> float - $float,<br> boolean - $boolean,<br> niekas - $null";
-        
-        ?>
+print $siandien;
+
+$dienos = $liepa+$rugpjutis+$rugsejis+$siandien;
+
+print "<br>";
+
+print $dienos;
+
+
+       
+
