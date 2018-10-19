771e16506c1fb52ace97c26cc0bd95bf863ffc27
mantasms <m.sakaitis@gmail.com>
2018-10-19 15:08:06 +0300

    Array #3 istraukti random produkta is saldytuvo

--- array.php
+++ array.php
@@ -1,18 +1,13 @@
 <?php
 $fridge = ["Jogurtas", "Kebabas", "Alus", "Sugede vaisiai", "Supuves avokadas"];
-$text = ' ';
-
-
-foreach ($fridge as $produktas) {
-
-    $text.=  $produktas . "<br>";
-}
+$i= rand(0, count ($fridge)-1);
+$text = $fridge [$i];
 
 ?>
 
 <html>
     <body>
-        <h1> Saldytuvo turinys: </h1>  
+        <!--<h1> Saldytuvo turinys: </h1>-->  
         <p> <?php print $text ; ?></p>
 
     </body>
