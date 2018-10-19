0afed94e7baeadb2b49946a285cedebb4de425da
mantasms <m.sakaitis@gmail.com>
2018-10-19 13:27:40 +0300

    ciklas for su break

--- ciklai.php
+++ ciklai.php
@@ -1,23 +1,33 @@
 <?php
-$x = 10;
-$y = 2;
-$text = "";
+$months = 12;
+$starting_money = 1000;
+$alga = 700;
+$islaidos = rand(0, 2000);
+$sum_islaidos = 0;
+$men_likutis = 0;
+$stop = null;
 
-for ($i = 0; $i<=10; $i++) {
-    if ($x>20) {
-        
-        $stop =' ciklas sustabdytas';
+
+for ($i = 1; $i <= $months; $i++) {
+    if ($starting_money<0)
+     {
+        $stop = " $i  menesi gali baigtis pinigai. Atsargiai !";
         break;
+        
     }
-    $x += $y;
-    $text .= "Dabartine X verte: $x <br>";
-    
+    $sum_islaidos += $islaidos;
+    $men = $i;
+    $v_islaidos = ($sum_islaidos / $men);
+    $men_likutis = $alga - $islaidos;
+    $starting_money += $men_likutis;
 }
 ?>
 
 <html>
     <body>
-        <h1> <?php print $text; ?></h1>  
+        <h1> <?php print "vidutines islaidos $v_islaidos "; ?></h1>  
+        <!--<h1> <?php print $men_likutis; ?></h1>-->
+        <h1> <?php print " likutis pabaigoje $starting_money "; ?></h1>
         <h1> <?php print $stop; ?></h1>
     </body>
 </html>
