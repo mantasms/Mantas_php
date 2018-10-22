bcaca9e0fcdd8e73d3e13b6097b61ea7d49190d0
mantasms <m.sakaitis@gmail.com>
2018-10-22 15:49:57 +0300

    day6 functions return su savo sugalvota funkcija

--- functions.php
+++ functions.php
@@ -1,17 +1,30 @@
 <?php
-
-function add ($x, $y) {
-return $x . '+' . $y. 'suma:' . ($x + $y);
+    $array1 = [2,3,4];
+    $array2 = [5,6,7];
+    
+/*
+ * @param integer array1 vienas masyvas su reiksmemis
+ * @param integer array2 kitas masyvas su reiksmemis
+ * @return integer suma array1 ir array2 
+ */
+function rank($array1, $array2) {
+    $a = array_sum($array1);
+    $b = array_sum($array2);
+    
+    return ($a + $b);
+    
 }
 ?>
 <html>
-    
+    <header>
+        
+    </header>
     <body>
 
-    <h1>
-     <?php print add(7,3); ?>
-    </h1>
+        <h1>
+            <?php print " suma " . rank($a, $b); ?>
+        </h1>
 
     </body>
 </html> 
-        
+
