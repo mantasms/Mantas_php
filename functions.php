522de3977606025e010c3cbd2b8afec200c1a0a3
mantasms <m.sakaitis@gmail.com>
2018-10-22 15:06:36 +0300

    day6 functions return

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function add ($x, $y) {
+return $x . '+' . $y. 'suma:' . ($x + $y);
+}
+?>
+<html>
+    
+    <body>
+
+    <h1>
+     <?php print add(7,3); ?>
+    </h1>
+
+    </body>
+</html> 
+        
