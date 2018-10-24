06fd44afa6bdeb355da50386068c4d481daa7a2f
mantasms <m.sakaitis@gmail.com>
2018-10-24 13:12:54 +0300

    DAY8 forms su f-ja

--- forms.php
+++ forms.php
@@ -1,21 +1,29 @@
 <?php
+$tekstas = 'ivesk skaiciu';
 
+function kvadratas($skaicius) {
+    $kvadratas = $skaicius ** 2;
+
+    return $kvadratas;
+}
+
+if (isset($_POST['skaicius'])) {
+    $skaicius = $_POST['skaicius'];
+    if (!empty($skaicius)) {
+        $tekstas = 'Atsakymas ' . kvadratas($skaicius);
+    }
+//    print $_POST['skaicius']**2;
+//    print $kvadratas;
+}
 ?>
 <html>
-    
-<form action="forms.php" method="POST">
-    Ka pakelti kvadratu: <input name="skaicius" type="text"/>
-    <input type="submit"/>
-</form>
+
+    <form action="forms.php" method="POST">
+        Ka pakelti kvadratu: <input name="skaicius" type="text"/>
+        <input type="submit"/>
+    </form>
+
+    <h1><?php print $tekstas; ?></h1>
 
 
 </html>
-<?php
-if (isset($_POST['skaicius'])) {
-    $skaicius = $_POST['skaicius']**2;
-    print $skaicius;
-    
-   
-
-}
-?>
